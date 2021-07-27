8934e80f3d71605ffe5f5ad322c364d0c4905e78
“laptopCarbon” <uquantum@hotmail.com>
2021-07-28 02:25:13 +0800

    ROUTER

---
+++ src/components/VFooter.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <p>shangxue education</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "VFooter"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/service.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <ul>
+    <li>list</li>
+    <li>list</li>
+    <li>list</li>
+    <li>list</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: "service"
+}
+</script>
+
+<style scoped>
+
+</style>
