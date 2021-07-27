eab4661e0ef7cc78172ee214d5f639576c1a7854
“laptopCarbon” <uquantum@hotmail.com>
2021-07-28 02:25:13 +0800

    ROUTER

---
+++ src/components/home.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h1>Home page</h1>
+  main body
+  <VFooter></VFooter>
+</div>
+</template>
+
+<script>
+import VFooter from "./VFooter";
+export default {
+  name: "home",
+  components: {VFooter},
+  // comments:{
+  //   footer //introduce others' link
+  // }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '../components/home'
+import user from '../components/user'
+import service from "../components/service";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:'/',
+      redirect:'/home'
+    },
+    {
+      path:'/home',
+      component:home
+    },
+    {
+      path:'/user',
+      component:user
+    },
+    {
+      path:'/service',
+      component:service
     }
   ]
 })
