27f58cad08591010a8c535461f17f60552cdf649
MountainWang <uquantum@hotmail.com>
2021-07-30 21:19:28 +0800

    element ui布局组件

---
+++ src/components/layout.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <h1>布局的使用</h1>
+  <el-row gutter="10">
+    <el-col :span="6"><div style="border:1px aquamarine solid">占用6份</div></el-col>
+    <el-col :span="6"><div style="border:1px aquamarine solid">占用6份</div></el-col>
+    <el-col :span="6"><div style="border:1px aquamarine solid">占用6份</div></el-col>
+    <el-col :span="6"><div style="border:1px aquamarine solid">占用6份</div></el-col>
+    <el-col :span="6">占有6份</el-col>
+    <el-col :span="6">占有6份</el-col>
+    <el-col :span="6">占有6份</el-col>
+  </el-row>
+</div>
+</template>
+
+<script>
+export default {
+  name: "layout"
+}
+</script>
+
+<style scoped>
+
+</style>
