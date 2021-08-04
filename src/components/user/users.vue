fe1aa4d12275639bc2ba5353dd52fc3c5d44d3e2
“laptopCarbon” <uquantum@hotmail.com>
2021-08-05 00:03:36 +0800

    分页功能

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -29,10 +29,34 @@
       <el-table-column label="电话" prop="mobile"></el-table-column>
       <el-table-column label="角色" prop="role_name"></el-table-column>
       <el-table-column label="状态" prop="mg_state">
-        <el-switch v-model></el-switch>
+        <template slot-scope="scope">
+<!--          自定义插槽-->
+<!--          {{scope.row}}-->
+          <el-switch v-model="scope.row.mg_state"></el-switch>
+        </template>
       </el-table-column>
-      <el-table-column label="操作"></el-table-column>
+      <el-table-column label="操作" width="180px">
+<!--        <template slot-scope="scope">-->
+        <template >
+<!--          {{scope.row}}-->
+          <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+          <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+          </el-tooltip>
+
+        </template>
+      </el-table-column>
     </el-table>
+    <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 3, 5]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+    </el-pagination>
   </el-card>
 </div>
 </template>
@@ -44,7 +68,9 @@ export default {
     return{
       queryInfo:{
         query:'',
+        //当前的页数
         pagenum:1,
+        //当前每页显示多少条
         pagesize:2
       },
       userList:[],
@@ -74,8 +100,21 @@ export default {
       }).catch(function (error){
         console.log(error)
       })
-
       // console.log(res)
+    },
+  //  监听pagesize改变的事件
+    handleSizeChange(newSize){
+      // console.log(newSize)
+      //根据获取的新值设置
+      this.queryInfo.pagesize=newSize;
+      //重新请求数据
+      this.getUserList()
+    },
+    //监听页码值改变的事件
+    handleCurrentChange(newPage){
+      console.log(newPage)
+      this.queryInfo.pagenum=newPage
+      this.getUserList()
     }
   }
 }
